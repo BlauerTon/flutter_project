40ef8eebd9b1edba51b6d3a4852c76f90387bf8d
kn_sid <sidneynduti@gmail.com>
2024-06-30 00:23:31 +0300

    Navigation to screen based on current login

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'signin_screen.dart';
+import 'splash_screen.dart';
 //import 'home_screen.dart';
 
 void main() async {
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'HomeSpace',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const SignInScreen(),
+      home:  SplashScreen(),
     );
   }
 }
