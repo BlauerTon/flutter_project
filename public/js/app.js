3e30f3edf8fff0e8f91452d2327435492699501b
Paris Buroko <127426195+p29ris@users.noreply.github.com>
2024-07-10 14:58:03 +0300

    Delete public directory

--- public/js/app.js
+++
@@ -1,6 +0,0 @@
-import './bootstrap';
-const hamBurger = document.querySelector(".toggle-btn");
-
-hamBurger.addEventListener("click", function () {
-  document.querySelector("#sidebar").classList.toggle("expand");
-});
